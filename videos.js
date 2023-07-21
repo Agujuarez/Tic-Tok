88c7c89b7ee03646dd11cf34c62f00eb3e613279
Agustin Juarez <agustin.juarez8483@gmail.com>
2023-07-21 18:34:20 -0300

    videos.js

---
+++ videos.js
@@ -0,0 +1,80 @@
+const { conectar, desconectar } = require('./db');
+
+// Función asincrónica para realizar la consulta SELECT
+async function selectVideos() {
+    return new Promise((resolve, reject) => {
+     const conexion= conectar();
+  
+      conexion.all('SELECT * FROM videos', (err, rows) => {
+        console.log('selectVideos',err,rows);
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+  
+      desconectar(conexion);
+    });
+  }
+
+  function crearTablaVideos() {
+    return new Promise((resolve, reject) => {
+      const conexion= conectar();
+      conexion.run(`
+        CREATE TABLE IF NOT EXISTS Videos (
+          ID INTEGER PRIMARY KEY,
+          Titulo TEXT,
+          Autor TEXT,
+          Fecha Creacion TEXT
+        )
+      `, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+  
+      desconectar(conexion);
+    });
+  }
+
+  async function crearVideo(id, nombre) {
+    return new Promise((resolve, reject) => {
+        const conexion= conectar();
+  
+      conexion.run('INSERT INTO videos (ID, Titulo, Autor, Fecha ) VALUES (?, ?)', [id, nombre, autor, fecha], function(err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(this.lastID);
+        }
+      });
+  
+      desconectar(conexion);
+    });
+  }
+
+  async function comprobarVideoExistente(id) {
+    return new Promise((resolve, reject) => {
+        const conexion= conectar();
+  
+      conexion.get('SELECT * FROM videos WHERE ID = ?', [id], (err, row) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(row !== undefined);
+        }
+      });
+  
+      desconectar(conexion);
+    });
+  }
+
+  module.exports = {
+    selectVideos,
+    crearTablaVideos,
+    crearVideo,
+    comprobarVideoExistente
+  }
